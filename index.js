19a239f5c3fdc89d09fed3675f0ff8f3236a5756
Jeet-Trueline <118980980+JeetBhuva@users.noreply.github.com>
2025-02-13 10:41:21 +0530

    13/02/2025
    query,params

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const http = require('http')
-const url = require('url')
-const querystring = require('querystring')
-const { add, sub } = require('./calculator')
+// const http = require('http')
+// const url = require('url')
+// const querystring = require('querystring')
+// const { add, sub } = require('./calculator')
 
-http.createServer(
+// http.createServer(
 
-    function (req, res) {
+//     function (req, res) {
 
-        const reqURL = req.url
-        // console.log(reqURL);
-        const urlParse = url.parse(reqURL)
-        // console.log(urlParse.query);
-        const querystringParse = querystring.parse(urlParse.query)
-        console.log(querystringParse);
+//         const reqURL = req.url
+//         // console.log(reqURL);
+//         const urlParse = url.parse(reqURL)
+//         // console.log(urlParse.query);
+//         const querystringParse = querystring.parse(urlParse.query)
+//         console.log(querystringParse);
 
-        const num1 = querystringParse.num1
-        const num2 = querystringParse.num2
+//         const num1 = querystringParse.num1
+//         const num2 = querystringParse.num2
 
-        if (reqURL.includes('/add')) {
-            res.write(add(num1, num2));
-        } else if (reqURL.includes('/sub')) {
-            res.write(sub(num1, num2))
-        }
+//         if (reqURL.includes('/add')) {
+//             res.write(add(num1, num2));
+//         } else if (reqURL.includes('/sub')) {
+//             res.write(sub(num1, num2))
+//         }
+//         res.end();
+//     }
 
-        res.end();
-    }
+// ).listen(8080)
 
-).listen(8080)
+
+const express = require('express')
+const calcu = require('./calculator')
+const app = express();
+
+app.get('/add/:num1/:num2', (req, res) => {
+    // const data = calcu.add(req.query.num1, req.query.num2)
+    const data = calcu.add(req.params.num1, req.params.num2)
+    res.send(data)
+})
+app.get('/sub/:num1/:num2', (req, res) => {
+    // const data = calcu.sub(req.query.num1, req.query.num2)
+    const data = calcu.sub(req.params.num1, req.params.num2)
+    res.send(data)
+})
+app.listen(8000)
